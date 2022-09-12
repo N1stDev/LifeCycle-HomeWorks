a9a5cb093db442e63069bf7649a0c0e36cb552af
Oleg Zhmelev <losdayver@yandex.ru>
2022-09-12 23:40:38 +0300

    исправления работы алгоритма

--- Task1_3/LongNumber.cs
+++ Task1_3/LongNumber.cs
@@ -1,20 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Task1_3
 {
     class LongNumber
     {
-        public string value;
+        public string _value;
+
+        public string value { get { return _value; } 
+            set {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (!("1234567890".Contains(value[i])))
+                    {
+                        _value = "0";
+                        return;
+                    }
+   
+                }
+
+                value = value.TrimStart('0');
+                _value = value;
+            } 
+        }
 
         public LongNumber(LongNumber other)
         {
-            value = new string(other.value);
+            _value = new string(other._value);
         }
 
         public LongNumber(string sequence)
@@ -24,13 +35,13 @@ namespace Task1_3
 
         public static bool operator >(LongNumber a, LongNumber b)
         {
-            if (a.value.Length > b.value.Length) return true;
-            else if (a.value.Length < b.value.Length) return false;
+            if (a._value.Length > b._value.Length) return true;
+            else if (a._value.Length < b._value.Length) return false;
 
-            for (int i = 0; i < a.value.Length; i++) 
+            for (int i = 0; i < a._value.Length; i++)
             {
-                if ((int)(a.value[i] - '0') > (int)(b.value[i] - '0')) return true;
-                else if ((int)(a.value[i] - '0') < (int)(b.value[i] - '0')) return false;
+                if ((int)(a._value[i] - '0') > (int)(b._value[i] - '0')) return true;
+                else if ((int)(a._value[i] - '0') < (int)(b._value[i] - '0')) return false;
             }
 
             return false;
@@ -38,16 +49,16 @@ namespace Task1_3
 
         public static bool operator <(LongNumber a, LongNumber b)
         {
-            return !(a > b);
+            return !(a > b) && a != b;
         }
 
         public static bool operator ==(LongNumber a, LongNumber b)
         {
-            if (a.value.Length != b.value.Length) return false;
+            if (a._value.Length != b._value.Length) return false;
             else
             {
-                for (int i = 0; i < a.value.Length; i++)
-                    if (a.value[i] != b.value[i]) 
+                for (int i = 0; i < a._value.Length; i++)
+                    if (a._value[i] != b._value[i])
                         return false;
             }
 
@@ -80,28 +91,29 @@ namespace Task1_3
 
             LongNumber a1, b1;
 
-            if (a.value.Length > b.value.Length)
+            if (a._value.Length > b._value.Length)
             { a1 = a; b1 = b; }
             else { a1 = b; b1 = a; }
 
             a1 = new LongNumber(a1);
             b1 = new LongNumber(b1);
 
-            b1.value = new string('0', a1.value.Length - b1.value.Length) + b1.value;
+            b1._value = new string('0', a1._value.Length - b1._value.Length) + b1._value;
 
             int mem = 0;
 
-            for (int i = b1.value.Length - 1; i >= 0; i--)
+            for (int i = b1._value.Length - 1; i >= 0; i--)
             {
-                int sub_result = (int)(a1.value[i] - '0') + (int)(b1.value[i] - '0') + mem;
+                int sub_result = (int)(a1._value[i] - '0') + (int)(b1._value[i] - '0') + mem;
 
                 if (sub_result >= 10)
-                { mem = 1; sub_result %= 10; } else mem = 0;
+                { mem = 1; sub_result %= 10; }
+                else mem = 0;
 
-                result.value = (sub_result).ToString() + result.value;
+                result._value = (sub_result).ToString() + result._value;
             }
 
-            if (mem == 1) result.value = (1).ToString() + result.value;
+            if (mem == 1) result._value = (1).ToString() + result._value;
 
             return result;
         }
@@ -128,55 +140,55 @@ namespace Task1_3
 
             LongNumber a1, b1;
 
-            if (a.value.Length > b.value.Length)
-            { a1 = a; b1 = b; }
-            else if (a > b)
-            { a1 = a; b1 = b; }
-            else 
-            { a1 = b; b1 = a; }
+            if (a > b)
+            { a1 = new LongNumber(a); b1 = new LongNumber(b); }
+            else if (a < b)
+            { a1 = new LongNumber(b); b1 = new LongNumber(a); }
+            else
+            {
+                result._value = "0";
+                return result;
+            }
 
-            a1 = new LongNumber(a1);
-            b1 = new LongNumber(b1);
 
-            int stop_at = a1.value.Length - b1.value.Length;
-
-            b1.value = new string('0', a1.value.Length - b1.value.Length) + b1.value;
+            b1._value = new string('0', a1._value.Length - b1._value.Length) + b1._value;
 
             // b1 меньше
 
-            for (int i = a1.value.Length - 1; i >= stop_at; i--)
+            for (int i = a1._value.Length - 1; i >= 0; i--)
             {
-                int sub_result = (int)(a1.value[i] - '0') - (int)(b1.value[i] - '0');
+                int sub_result = (int)(a1._value[i] - '0') - (int)(b1._value[i] - '0');
 
                 if (sub_result < 0)
                 {
                     for (int j = i; j >= 0; j--)
                     {
 
-                        int buff = (int)(a1.value[j] - '0');
+                        int buff = (int)(a1._value[j] - '0');
 
                         if (buff == 0)
                         {
-                            StringBuilder sb = new StringBuilder(a1.value);
+                            StringBuilder sb = new StringBuilder(a1._value);
                             sb[j] = '9';
-                            a1.value = sb.ToString();
+                            a1._value = sb.ToString();
                         }
                         else
                         {
                             buff -= 1;
-                            StringBuilder sb = new StringBuilder(a1.value);
+                            StringBuilder sb = new StringBuilder(a1._value);
                             sb[j] = Convert.ToChar(buff.ToString());
-                            a1.value = sb.ToString();
+                            a1._value = sb.ToString();
                             sub_result += 10;
                             break;
-                   
+
                         }
                     }
                 }
-                
-                result.value = (sub_result).ToString() + result.value;
+
+                result._value = (sub_result).ToString() + result._value;
             }
 
+            result._value = result._value.TrimStart('0');
             return result;
         }
     }
--- Task1_3/Program.cs
+++ Task1_3/Program.cs
@@ -4,11 +4,11 @@
     {
         private static void Main(string[] args)
         {
-            LongNumber a = new LongNumber("12");
-            LongNumber b = new LongNumber("12");
+            LongNumber a = new LongNumber("99999");
+            LongNumber b = new LongNumber("00010000");
 
             Console.WriteLine(a == b);
-            Console.WriteLine((a - b).value);
+            Console.WriteLine((a - b)._value);
         }
     }
 
