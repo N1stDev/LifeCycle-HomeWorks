6e1e80b9cfb16ee5e60a613bbd55d4c7a1cfebd6
N1stDev <nist.dev@yandex.ru>
2022-09-30 18:20:58 +0300

    сделал 4 и 3 задачи

---
+++ Task4_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task4_3
+{
+    class Program
+    {
+        static int GetSum(params int[] nums)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+
+            return sum;
+
+        }
+        static int Main()
+        {
+            int sum = GetSum(1, 2, 3, 4, 5, 6, 7, 8);
+
+            Console.WriteLine(sum);
+
+            return 0;
+        }
+    }
+}
---
+++ Task4_4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task4_4
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+
+        }
+        static int Main()
+        {
+            int a = 100;
+            int b = -100;
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            return 0;
+        }
+    }
+}
