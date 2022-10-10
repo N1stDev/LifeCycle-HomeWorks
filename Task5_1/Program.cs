839ca671e8cb4a81468c548ab7488415e66d4808
MagicEaster <ToraCooky@gmail.com>
2022-10-10 21:52:42 +0300

    add task 5-1

---
+++ Task5_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_1
+{
+
+    public class Enum1
+    {
+       enum Days { Monday=1 , Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+
+
+        static void Main(string[] args)
+        {
+            Type weekdays = typeof(Days);
+
+            Console.WriteLine("Enter weekday`s number: ");
+
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} = {1}", i, Enum.GetName(weekdays, i), "d");
+            
+                    }
+    }
+
+}
