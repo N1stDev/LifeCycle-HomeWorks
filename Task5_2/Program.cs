2e0adc17c48900df290ad59df8bee579095c891e
MagicEaster <ToraCooky@gmail.com>
2022-10-10 22:49:00 +0300

    continue

--- Task5_2/Program.cs
+++ Task5_2/Program.cs
@@ -9,7 +9,7 @@ namespace Task5_2
     internal class Program
     {
         [Flags]
-        enum Shops
+        enum Goods
         {
             Food = 1,
             Electronics = 2,
@@ -20,12 +20,17 @@ namespace Task5_2
 
         static void Main(string[] args)
         {
-            var floor1 = Shops.Food|Shops.Electronics;
-            var floor2 = Shops.Books|Shops.Clothes|Shops.Electronics;
-            var floor3 = Shops.Other|Shops.Books;
+            var floor1 = Goods.Food|Goods.Electronics;
+            var floor2 = Goods.Books|Goods.Clothes|Goods.Electronics;
+            var floor3 = Goods.Other|Goods.Books;
 
-            Shops[] mall = { floor1, floor2, floor3 };
+            Goods[] mall = { floor1, floor2, floor3 };
 
+            for (int ctr = 0; ctr <mall.Length; ctr++)
+                Console.WriteLine("On floor {0} u can buy food: {1}",
+                                  ctr + 1,
+                                  (mall[ctr] & Goods.Food) == Goods.Food ?
+                                     "Yes" : "No");
         }
     }
 }
